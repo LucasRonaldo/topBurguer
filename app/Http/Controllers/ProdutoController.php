499493222ae15c1edd388c13dc2aa2375b3494c8
LucasRonaldo <lucasronaldoalvesdasilva@gmail.com>
2024-04-02 15:38:23 -0300

    projeto atualizado

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -7,20 +7,33 @@ use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    public function index(){
-        $produtos =Produto::all();
+    public function index()
+    {
+        $produtos = Produto::all();
 
         $produtosComImagem = $produtos->map(function ($produto) {
 
-            return[
-                'nome'=> $produto->nome,
-                'preco'=> $produto->preco,
-                'ingredientes'=> $produto->ingredientes,
-                'imagem'=> asset('storage/'. $produto->imagem),
-    
-            ];
-           
-        });
+            return [
+                'nome' => $produto->nome,
+                'preco' => $produto->preco,
+                'ingredientes' => $produto->ingredientes,
+                'imagem' => asset('storage/' . $produto->imagem),
 
-       
-    }}
+            ];
+        });
+    }
+
+    public function store(Request $request){
+        $produtoData = $request->all();
+
+        if($request->hasFile('imagem')){
+            $imagem = $request->file('imagem');
+            $nomeImagem = time().'.'.$imagem->getClientOriginalExtension();
+            $caminhoImagem = $imagem->storeAs('imagens/produtos', $nomeImagem, 'public');
+            $produtoData['imagem'] = $caminhoImagem;
+        }
+
+        $produto = Produto::create($produtoData);
+        return response()->json(['produto'=>$produto]);
+    }
+}
