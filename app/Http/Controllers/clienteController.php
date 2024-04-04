4a5d8d39ccd8506e918a5092a412d88c1a2e1bba
LucasRonaldo <lucasronaldoalvesdasilva@gmail.com>
2024-04-04 15:56:25 -0300

    Atualizando

--- app/Http/Controllers/clienteController.php
+++ app/Http/Controllers/clienteController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Cliente;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class clienteController extends Controller
 {
     
-    public function cadastroCliente(Request $request){
-        $cliente = Cliente::create([
-            'nome'=> $request->nome,
-            ''
-        ])
+    public function cadastrarCliente(Request $request){
+        $clientes = Cliente::create([
+            'nome' => $request->nome,
+            'telefone' => $request->telefone,
+            'endereco' => $request->endereco,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Cliente foi cadastrado com sucesso.",
+            "data" => $clientes
+        ], 200);
     }
 }
